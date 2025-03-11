a82ad66429dee64272fe4c53cad8ba537bee8b85
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-03-11 17:01:20 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,6 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+iris = load_iris()
